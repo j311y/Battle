47e9aaa20ad03d173d02ed1c16691e47c0bb15f0
Paula <pmuldoon@umich.edu>
2017-04-28 15:02:00 +0100

    added play_p2 and attack_p2 views

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -12,15 +12,23 @@ feature 'Hit points' do
     expect(page).to have_content 'Blue: 30HP'
   end
 
-  scenario 'get confirmation of attack' do
+  # scenario 'get confirmation of attack' do
+  #   sign_in_and_play
+  #   click_button('Attack Player 1')
+  #   expect(page).to have_content "It's super effective!"
+  # end
+
+  scenario 'reduce P1 hit points by 10' do
     sign_in_and_play
-    click_button('Attack')
-    expect(page).to have_content "It's super effective!"
+    click_button('Attack Player 2!')
+    click_button('Back')
+    click_button('Attack Player 1!')
+    expect(page).not_to have_content
   end
 
   scenario 'reduce P2 hit points by 10' do
     sign_in_and_play
-    click_button('Attack')
+    click_button('Attack Player 2!')
     click_button('Back')
     expect(page).not_to have_content 'Blue: 30HP'
     expect(page).to have_content 'Blue: 20HP'

b6e3dc269879f772ead60a33f1c6f1be2adb3995
Jonathan Elliot <jonathan.j.elliot@gmail.com>
2017-04-28 12:19:46 +0100

    added array to game initialize method and added 2 player methods

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,4 +11,16 @@ describe Game do
       game.attack(player_2)
     end
   end
+
+  describe '#player_1' do
+    it 'retrieves the first player' do
+      expect(game.player_1).to eq player_1
+    end
+  end
+
+  describe '#player_2' do
+    it 'retrieves the second player' do
+      expect(game.player_2).to eq player_2
+    end
+  end
 end

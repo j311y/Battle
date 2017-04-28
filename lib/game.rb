106d732e54ebef9f7bf7824e2be2430bcb32e47a
Paula <pmuldoon@umich.edu>
2017-04-28 11:44:56 +0100

    fully intialized Game class, green tests

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
 
   def initialize(player_1, player_2)
+    @player_1 = player_1
+    @playr_2 = player_2
   end
 
   def attack(player)
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,19 +5,10 @@ describe Game do
   let(:player_1) { double :player }
   let(:player_2) { double :player }
 
-  describe '#initialize' do
-    it 'initializes with 2 players' do
-      expect game(player_1, player_2)
-    end
-  end
-
   describe '#attack' do
     it 'damages other player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
     end
   end
-
-
-
 end
